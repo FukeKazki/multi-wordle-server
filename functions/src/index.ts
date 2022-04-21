c1f1ba9413b041f39d97978c20015fad607d1388
FukeKazki <kazkichi0906@gmail.com>
2022-04-21 18:06:11 +0900

    Update: cors許可

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { router } from "./infrastructure/router";
 import * as functions from "firebase-functions";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(router);
 
 export const wordle = functions
